77a306072c4f8c3da4f2b4edcff68f4338596c54
Suede Young <suedeyoung1@gmail.com>
2019-04-24 04:13:54 -0500

    updating script to check for /home/steamcmd and create it if needed

--- installsteamcmd.py
+++ installsteamcmd.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python
 import subprocess
+import os
+from pathlib2 import Path
 
-print("Greetings! This script is still in beta so please let me know if there is any issues with the script itself.\nSo far, it seems that things are just barely getting started and this is my first full blown python script so I'm sorry if it looks bad")
-
+print("Greetings! This script is still in beta so please let me know if there is any issues with the script itself.\nSo far, it seems that things are just barely getting started and this is my first full blown python script so I'm sorry if it looks bad \n")
 subprocess.call(["ls", "-lah"])
-print("Completed ls -lah subprocess call")
+
+print("Completed ls -lah subprocess call \n")
+
+
+path = os.getcwd()
+print("The current path of this system is %s \n" % path)
+
+
+print("Checking if /home/steamcmd is available... \n")
+
+
+my_file = Path ("/home/steamcmd")
+if my_file.is_dir():
+    os.chdir("/home/steamcmd")
+    print("/home/steamcmd is already on the server, moving on.. \n")
+else:
+    print("Not finding /home/steamcmd.. making folder")
+    os.mkdir("/home/steamcmd")
+    print("Folder should be created, testing it now \n")
+    os.chdir("/home/steamcmd")
+    print("Successfully created /home/steamcmd")
+
